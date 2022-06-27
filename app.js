dbfe8d6cb880d1682d09dbc511431175fdf05be3
German Iacona <german.iacona@gmail01~>
2022-06-27 01:08:21 -0300

    actualizacion

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath) );
 
 app.listen(3000, () => {
     console.log("Servidor escuchando en el puerto 3000");
 });
+
+//envia el archivo al navegador
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
